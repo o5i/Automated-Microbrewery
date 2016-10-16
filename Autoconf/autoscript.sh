dd10105c1c5fe5ae64174a6a967ad4ff02e63d89
o5i <oskar.perger@gmail.com>
2016-10-16 12:53:34 +0200

    up

--- Autoconf/autoscript.sh
+++ Autoconf/autoscript.sh
@@ -1,2 +1,3 @@
-teensy_loader_cli -mmcu=mk20dx256 -w -s -v /media/shared/Mirco_Firmware/*.hex
-rm /media/shared/Mirco_Firmware/*.hex
+cd /media/shared/GIT/
+git pull
+/media/shared/GIT/Autoconf/Progs/teensy_loader_cli -mmcu=mk20dx256 -w -s -v /media/shared/Autoconf/Micro_Firmware/*.hex
